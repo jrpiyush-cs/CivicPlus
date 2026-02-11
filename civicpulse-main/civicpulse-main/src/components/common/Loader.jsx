e6b73861ddfbde329caa217f52ab880040c1c7e8
jrpiyush-cs <piyush.shamkant.patil@gmail.com>
2026-02-12 00:10:02 +0530

    Delete civicpulse-main/civicpulse-main directory

--- civicpulse-main/civicpulse-main/src/components/common/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Loader = () => (
-  <div className="loader flex justify-center items-center h-16">
-    <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
-  </div>
-);
-
-export default Loader;
--- civicpulse-main/civicpulse-main/src/utils/constants.jsx
+++
@@ -1,7 +0,0 @@
-export const GNEWS_API_URL = "https://gnews.io/api/v4/search";
-export const BLOGS_COLLECTION = "blogs";
-export const CHATS_COLLECTION = "chats";
-export const DEFAULT_TOPIC = "technology";
-export const MAX_BLOG_LENGTH = 500;
-export const CHAT_MESSAGE_LIMIT = 50;
-
